9697e9c81908ba007dfef98023028365344f7cfd
Clarice Regina <186510882+clariceregina@users.noreply.github.com>
2026-01-03 23:20:54 +0000

    Adding SHEBANG and a comment

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print("Olá, Mundo!!")
+#!/usr/bin/env python3
+
+# The comment below indicates where python is in my Linux. This type of comment is a SHEBANG
+
+# This program prints 
+print("Hello World!!")
